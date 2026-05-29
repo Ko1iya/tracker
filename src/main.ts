51d39b9be3b41009c606b3f5e1ec3f0cab603efa
nikolay.fialkovskiy <nikolay.fialkovsky@anywayanyday.com>
2026-05-29 17:24:28 +0800

    поправил cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    // localhost + приватные подсети (192.168.x.x, 10.x.x.x) на порту dev-сервера фронта,
+    // чтобы открывать приложение с телефона по сетевому IP
+    origin: /^http:\/\/(localhost|127\.0\.0\.1|192\.168\.\d{1,3}\.\d{1,3}|10\.\d{1,3}\.\d{1,3}\.\d{1,3}):5173$/,
   });
   app.useGlobalPipes(
     new ValidationPipe({
